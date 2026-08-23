e3f0916bf246748b1ead5a79ab4b83b41733a060
imguest24897-alt <161818573+imguest24897-alt@users.noreply.github.com>
2026-08-23 06:04:53 +0300

    add a alpha variable thing to the version generator thingy

--- lk2nd/scripts/describe-version.sh
+++ lk2nd/scripts/describe-version.sh
@@ -16,11 +16,20 @@ git_dirty() {
 	fi
 }
 
+# The variable that basically lets the thing know that it's a alpha version idk im used to fish instead of sh or bash
+export isAlpha=true
+alphaVer() {
+	if [ "$isAlpha" = true ];
+	then
+		echo "-alpha"
+	fi
+}
+
 # Echo version of the project based on git
 get_version_string() {
 	if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1
 	then
-		echo "$(date +%Y.%m.%d)-alpha"
+		echo "$(date +%Y.%m.%d)$(alphaVer)"
 		return
 	fi
 
@@ -29,18 +38,18 @@ get_version_string() {
 	last_tag=$(git tag --sort=-taggerdate --merged | head -n1)
 	if [ -z "$last_tag" ]
 	then
-		echo "$(date +%Y.%m.%d)-alpha$(git_dirty)"
+		echo "$(date +%Y.%m.%d)$(alphaVer)$(git_dirty)"
 		return
 	fi
 
 	last_tag_ref=$(git rev-list -n1 "$last_tag")
 	if [ "$last_tag_ref" = "$(git rev-list -n1 HEAD)" ]
 	then
-		echo "$last_tag$(git_dirty)"
+		echo "$last_tag$(alphaVer)$(git_dirty)"
 		return
 	fi
 
-	echo "$(date +%Y.%m.%d)-alpha$(git_dirty)"
+	echo "$(date +%Y.%m.%d)$(alphaVer)$(git_dirty)"
 }
 
 get_version_string
